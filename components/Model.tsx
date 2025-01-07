0c1bec2b08b84320a8bdbc6689c4fcb85bed45c7
NAVIN0507 <navinofficial2005@gmail.com>
2025-01-07 10:16:39 +0530

    nodeMailer

--- components/Model.tsx
+++ components/Model.tsx
@@ -1,10 +1,21 @@
 "use client"
 
-import React , {Fragment, useState}from 'react'
-import { Description, Dialog, DialogPanel, DialogTitle, Transition , Overlay} from '@headlessui/react'
+import React , {FormEvent, Fragment, useState}from 'react'
+import { Description, Dialog, DialogPanel, DialogTitle, Transition } from '@headlessui/react'
+import Image from 'next/image';
 
 const Model = () => {
     let [isOpen, setIsOpen] = useState(false);
+    const [isSubmiting, setIsSubmiting] = useState(false);
+    const [email, setemail] = useState('');
+    const handleSubmit = async (e:FormEvent<HTMLFormElement>) =>{
+        e.preventDefault();
+        setIsSubmiting(true);
+
+        setIsSubmiting(false)
+        setemail('');
+        closeModel();
+    }
     const openModel = ()=>setIsOpen(true);
     const closeModel =()=> setIsOpen(false);
   return (
@@ -12,19 +23,53 @@ const Model = () => {
    <button type='button'  onClick={openModel}className='btn'>Track</button>
    <Dialog open={isOpen} onClose={() => setIsOpen(false)} className="dialog-container">
         <div className="fixed inset-0 flex w-screen items-center justify-center p-4">
-          <DialogPanel className="max-w-lg space-y-4 border bg-white p-12">
-            <DialogTitle className="font-bold">Deactivate account</DialogTitle>
-            <Description>This will permanently deactivate your account</Description>
-            <p>Are you sure you want to deactivate your account? All of your data will be permanently removed.</p>
-            <div className="flex gap-4">
-              <button onClick={() => setIsOpen(false)}>Cancel</button>
-              <button onClick={() => setIsOpen(false)}>Deactivate</button>
+            <span className='inline-block h-screen align-middle' aria-hidden="true"/>
+<Transition.Child as={Fragment} enter='ease-out duration-300' enterFrom='opacity-0 scale-95' enterTo='opacity-100 scale-100' leave='ease-in duration-200' leaveFrom='opacity-100 scale-100' leaveTo='opacity-0 scale-95'>
+    <div className='dialog-content'>
+        <div className='flex flex-col'>
+            <div className='flex justify-between'>
+                <div className='p-3 border border-gray-200 rounded-10'>
+                    <Image 
+                    src="/assets/icons/logo.svg"
+                    alt='logo'
+                    width={28}
+                    height={28}
+                    /> 
+                </div>
+                <Image
+                src="/assets/icons/x-close.svg"
+                alt='close'
+                width={24}
+                height={24}
+                className='cursor-pointer'
+                onClick={closeModel }
+                />
             </div>
-          </DialogPanel>
+            <h4 className='dialog-head_text'>Stay updated with product pricing alerts right in your inbox!</h4>
+            <p className='text-sm text-gray-600 mt-2'>Never miss a bargaing again with out timely alerts!</p>
+        </div>
+        <form action="" className='flex flex-col mt-5' onSubmit={handleSubmit}>
+            <label htmlFor="email" className='text-sm font-medium text-gray-700'>Email Address</label>
+            <div className='dialog-input_container'>
+                <Image 
+                src="/assets/icons/mail.svg"
+                alt='mail'
+                width={18}
+                height={18}
+                />
+                <input type="email" id='email' required placeholder='Enter your email address' className='dialog-input' value={email} onChange={(e)=> setemail(e.target.value)} />
+            </div>
+            <button type='submit' className='dialog-btn'>
+                {isSubmiting ? 'Submitting...' : 'Track'}
+            </button>
+        </form>
+    </div>
+
+</Transition.Child>
         </div>
       </Dialog>
    </>
   )
 }
 
-export default Model
+export default Model 
--- lib/actions/index.ts
+++ lib/actions/index.ts
@@ -4,6 +4,7 @@ import Product from "../models/product.model";
 import { connectToDB } from "../mongoose";
 import { scrapAmazonProduct } from "../scraper";
 import { getAveragePrice, getHighestPrice, getLowestPrice } from "../utils";
+import { User } from "@/types";
 
 export async function scrapAndStoreProduct(productUrl: string){
     if(!productUrl) return;
@@ -71,4 +72,18 @@ try {
     
 }
 
+}
+export async function addUserEmailProduct(productId : string,  userEmail : string){
+try {
+    const product = await Product.findById(productId);
+    if(!product) return;
+    const userExists = product.users.some((user : User)=> user.email === userEmail);
+    if(!userExists){
+        product.users.push({email : userEmail});
+        await product.save();
+    const emailContent = generateEmailBody(product , "WELCOME")
+    }
+} catch (error) {
+    console.log(error)
+}
 }
